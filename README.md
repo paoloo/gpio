0454b66f05f62cf3c5ec792ccc30ddb0f90e7b43
paoloo <paolo@fisica.ufc.br>
2016-05-20 02:03:30 -0300

    working version

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# gpio
-A simple, pure erlang, raspberry pi user-space gpio library
+#πGPIO
+A simple, pure erlang implementation of a module for Raspberry Pi's General Purpose 
+Input/Output (GPIO), using the standard Linux kernel interface for user-space, sysfs,
+available at /sys/class/gpio/.
+
+This module was created to try robotics on raspberry Pi using erlang and have not tests,
+error checking or fancy outputs. It was created for my own use and I'm releasing as it
+may be useful for someone.<br>I hope you enjoy!
+
+## Requirements
+- **Raspbian** GNU/Linux;
+- **erlang** 17+, `apt-get install erlang` solve it;
+- **root**, to access "/sys/class/gpio/" files.
+- **rebar**, to speed things up (`wget https://raw.github.com/wiki/rebar/rebar/rebar && chmod u+x rebar`)
+
+## Usage
+### on projects
+- add to dependencies of your rebar.config:
+```
+{deps, [
+    {gpio, "", {git, "git://github.com/paoloo/gpio.git"}},
+    ... % other dependencies goes here
+]}.
+```
+- then just compile:
+```
+$ rebar compile
+```
+
+### on REPL
+```
+$ sudo erl
+Erlang/OTP 17 [erts-6.2] [source] [async-threads:10] [kernel-poll:false]
+
+Eshell V6.2  (abort with ^G)
+1> c(gpio).
+{ok,gpio}
+2> L0 = gpio:init(23, out).
+<0.43.0>
+3> L1 = gpio:init(22, in).
+<0.48.0>
+4> gpio:write(L0, 1).
+ok
+5> gpio:read(L1).
+"1"
+```
+
+## Documentation
+- **init(Pin, Direction)** Initialize a **pin** as *output* or *input*, according to **Direction**(an atom). It returns a *Reference* to *Pin*.
+- **init(Pin)** A shortcut to initialize *pin* as *output*. It, also, returns a *Reference* to *Pin*.
+- **stop(Ref)** Stop using and release th **pin** referenced as **Ref**. It returns an atom, **ok**.
+- **write(Ref, Val)** Writes **Val**, 1 or 0, into the **pin** initialized and referenced as **Ref**.
+- **read(Ref)** Reads a value(1 or 0) from **pin** referenced as **Ref** and returns it.
+
+For the official erlang-style documentation, run:
+```
+$ erl -noshell -run edoc_run application 'gpio' '"."' '[]'
+```
+
+## Tested on
+- Erlang/OTP 17 erts-6.2 in GNU/Linux Raspian 8.0 (jessie) kernel 4.1.19+ armv61 on Raspberry B+.
+
+## License
+[MIT License](LICENSE.md) © 2015-2016 Paolo Oliveira.
---
+++ src/gpio.erl
@@ -0,0 +1,82 @@
+%% @author Paolo Oliveira <paolo@fisica.ufc.br>
+%% @copyright 2015-2016 Paolo Oliveira (license MIT)
+%% @version 1.0.0
+%% @doc
+%% A simple, pure erlang implementation of a module for <b>Raspberry Pi's General Purpose
+%% Input/Output</b> (GPIO), using the standard Linux kernel interface for user-space, sysfs,
+%% available at <b>/sys/class/gpio/</b>.
+%% @end
+
+-module(gpio).
+-export([init/1, init/2, handler/2, read/1, write/2, stop/1]).
+-author('Paolo Oliveira <paolo@fisica.ufc.br>').
+
+%% API
+
+% @doc: Initialize a Pin as input or output.
+init(Pin, Direction) ->
+  Ref = configure(Pin, Direction),
+  Pid = spawn(?MODULE, handler, [Ref, Pin]),
+  Pid.
+
+% @doc: A shortcut to initialize a Pin as output.
+init(Pin) ->
+  init(Pin, out).
+
+% @doc: Stop using and release the Pin referenced as file descriptor Ref.
+stop(Ref) ->
+  Ref ! stop,
+  ok.
+
+% @doc: Read from an initialized Pin referenced as the file descriptor Ref.
+read(Ref) ->
+  Ref ! {recv, self()},
+  receive
+    Msg ->
+      Msg
+  end.
+
+% @doc: Write value Val to an initialized Pin referenced as the file descriptor Ref.
+write(Ref, Val) ->
+  Ref ! {send, Val},
+  ok.
+
+%% Internals
+
+configure(Pin, Direction) ->
+  {ok, RefExport} = file:open("/sys/class/gpio/export", [write]),
+  file:write(RefExport, integer_to_list(Pin)),
+  file:close(RefExport),
+  {ok, RefDirection} = file:open("/sys/class/gpio/gpio" ++ integer_to_list(Pin) ++ "/direction", [write]),
+  case Direction of
+    in -> file:write(RefDirection, "in");
+    out -> file:write(RefDirection, "out")
+  end,
+  file:close(RefDirection),
+  {ok, RefVal} = file:open("/sys/class/gpio/gpio" ++ integer_to_list(Pin) ++ "/value", [read, write]),
+  RefVal.
+
+release(Pin) ->
+  {ok, RefUnexport} = file:open("/sys/class/gpio/unexport", [write]),
+  file:write(RefUnexport, integer_to_list(Pin)),
+  file:close(RefUnexport).
+
+% @doc: Message passing interface, should not be used directly, it is present for debugging purpose.
+handler(Ref, Pin) ->
+  receive
+    {send, Val} ->
+      file:position(Ref, 0),
+      file:write(Ref, integer_to_list(Val)),
+      handler(Ref, Pin);
+    {recv, From} ->
+      file:position(Ref, 0),
+      {ok, Data} = file:read(Ref, 1),
+      From ! Data,
+      handler(Ref, Pin);
+    stop ->
+      file:close(Ref),
+      release(Pin),
+      ok
+   end.
+
+%% End of Module.
